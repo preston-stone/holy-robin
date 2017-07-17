745c02e84e3e89160f531d298d1070548e93ef43
Preston Stone <prestonmarkstone@gmail.com>
2017-07-17 17:34:01 -0400

    Added array filtering
    Added array filtering to remove new lines and empty elements

--- js/what-would-robin-say.js
+++ js/what-would-robin-say.js
@@ -1,23 +1,29 @@
-
 function getWikiData() {
-  var wURL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&titles=List_of_exclamations_by_Robin&prop=revisions&rvprop=content&rvsection=2&indexpageids&callback=?';
-  
- $.getJSON(wURL ,function(data) {
-    var hash = data;
-     var pageid = data["query"]["pageids"][0];
-     var content = data["query"]["pages"][pageid]["revisions"][0]["*"];
-      getSaying(content);  
-  });
+    var wURL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&titles=List_of_exclamations_by_Robin&prop=revisions&rvprop=content&rvsection=2&indexpageids&callback=?';
+
+    $.getJSON(wURL, function(data) {
+        var pageid = data["query"]["pageids"][0];
+        var content = data["query"]["pages"][pageid]["revisions"][0]["*"];
+        getSaying(content);
+    });
 }
 
-function getSaying(content){
- var scontent = content.split("{{div col|3}}").pop().split("{{div col end}}").shift();
- var rSayings = scontent.split("*")
- var saying = rSayings[Math.floor(Math.random() * rSayings.length)].trim();
-  $("#rSaying").html(saying+'!');
+function getSaying(content) {
+    var scontent = content.split("{{div col|3}}").pop().split("{{div col end}}").shift();
+    var rSayings = scontent.split("*").map(function(idx) {
+        return idx.trim();
+    }).filter(function(n) {
+        return n != ""
+    });
+
+    console.log(rSayings);
+    var saying = rSayings[Math.floor(Math.random() * rSayings.length)].trim();
+    $("#rSaying").html(saying + '!');
 }
 
 $(document).ready(function() {
-  getWikiData();
-  $("#reload-link").click(function(){getWikiData();});
+    getWikiData();
+    $("#reload-link").click(function() {
+        getWikiData();
+    });
 })
