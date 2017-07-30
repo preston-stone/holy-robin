45d403b5ace18113da630eedd36e0c5438a57603
Preston Stone <prestonmarkstone@gmail.com>
2017-07-30 15:20:09 -0400

    Webpack/Express additions

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+
+module.exports = {
+   plugins: [
+    new UglifyJSPlugin()
+],
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+};
